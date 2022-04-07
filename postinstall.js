40a38b50b20af488f500709da4a18a866a8436a1
WilliamFalci <falci.williampeter@gmail.com>
2022-04-07 17:59:50 +0200

    more test on postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -15,6 +15,7 @@ if(Object.keys(packageJSON).includes('scripts')){
   packageJSON.scripts.pgc_compare = "yarn --cwd ./node_modules/node-pg-compare pgc-compare"
 }
 
+console.log(process.env)
 fs.writeFile(`${process.env.PROJECT_CWD}/package.json`, packageJSON, 'utf8', function (err) {
   if (err) return console.log(err,process.env);
 });
