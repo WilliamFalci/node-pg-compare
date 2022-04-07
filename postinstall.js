ba93ef8fff80e05e23c4e7758e952677aa22d848
WilliamFalci <falci.williampeter@gmail.com>
2022-04-07 17:56:36 +0200

    more test on postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -2,5 +2,19 @@ const fs = require('fs');
 console.log('#### NODE PG COMPARE POST INSTALL')
 fs.createReadStream('.blank.env').pipe(fs.createWriteStream('.env'));
 
-console.log(process.env)
-console.log(process.env.PROJECT_CWD)
+let packageRawData = fs.readFileSync(`${process.env.PROJECT_CWD}/package.json`);
+let packageJSON = JSON.parse(packageRawData);
+
+
+if(Object.keys(packageJSON).includes('scripts')){
+  packageJSON.scripts.pgc_tables = "yarn --cwd ./node_modules/node-pg-compare pgc-tables"
+  packageJSON.scripts.pgc_compare = "yarn --cwd ./node_modules/node-pg-compare pgc-compare"
+}else{
+  packageJSON.scripts = {}
+  packageJSON.scripts.pgc_tables = "yarn --cwd ./node_modules/node-pg-compare pgc-tables"
+  packageJSON.scripts.pgc_compare = "yarn --cwd ./node_modules/node-pg-compare pgc-compare"
+}
+
+fs.writeFile(`${process.env.PROJECT_CWD}/package.json`, packageJSON, 'utf8', function (err) {
+  if (err) return console.log(err);
+});
