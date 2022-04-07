e9ebc1640e4b6d03721e52a2e164d0a281b6077c
WilliamFalci <falci.williampeter@gmail.com>
2022-04-07 18:35:04 +0200

    added env command depend from os

--- postinstall.js
+++ postinstall.js
@@ -4,17 +4,30 @@ fs.createReadStream('.blank.env').pipe(fs.createWriteStream('.env'));
 
 let packageRawData = fs.readFileSync(`${process.env.INIT_CWD}/package.json`);
 let packageJSON = JSON.parse(packageRawData);
-
+let command
+switch(process.platform){
+  case 'openbsd':
+  case 'freebsd':
+  case 'linux':
+    command = 'xdg-open';
+    break;
+  case 'win32':
+    command = 'start';
+    break;
+  case 'darwin':
+    command = 'open'
+    break;
+}
 
 if(Object.keys(packageJSON).includes('scripts')){
   packageJSON.scripts.pgc_tables = "yarn --cwd ./node_modules/node-pg-compare pgc-tables"
   packageJSON.scripts.pgc_compare = "yarn --cwd ./node_modules/node-pg-compare pgc-compare"
-  packageJSON.scripts.pgc_env = "yarn --cwd ./node_modules/node-pg-compare pgc-env"
+  packageJSON.scripts.pgc_env = `${command} ./node_modules/node-pg-compare/.env`
 }else{
   packageJSON.scripts = {}
   packageJSON.scripts.pgc_tables = "yarn --cwd ./node_modules/node-pg-compare pgc-tables"
   packageJSON.scripts.pgc_compare = "yarn --cwd ./node_modules/node-pg-compare pgc-compare"
-  packageJSON.scripts.pgc_env = "yarn --cwd ./node_modules/node-pg-compare pgc-env"
+  packageJSON.scripts.pgc_env = `${command} ./node_modules/node-pg-compare/.env`
 }
 
 fs.writeFile(`${process.env.INIT_CWD}/package.json`, JSON.stringify(packageJSON), 'utf8', function (err) {
